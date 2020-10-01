f1c347ad9a30f04c6f5ecadb92ffb6bae81f9e97
nngocjade <nngocjade@gmail.com>
2020-10-01 11:01:00 +0800

    created rainbow color text

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,17 @@
-const colors = ["green", "red","rbba(133,122,200)", "#f15025"];
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
-btn.addEventListener("click", function(){
- console.log(document.body);
+btn.addEventListener("click", function () {
+  //get random number between 0 and 3 colors[]
+  const randomNumber = getRandomNumber();
+  console.log(randomNumber);
+
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
 });
+
+function getRandomNumber() {
+ //math floor will round to the nearest integer
+  return Math.floor(Math.random() * colors.length);
+}
