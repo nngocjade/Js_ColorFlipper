4c1006b10d291e58347068b16e936684a17215c8
nngocjade <nngocjade@gmail.com>
2020-10-01 13:42:15 +0800

    adding logic to random hex color generator

--- dist/js/hex.js
+++ dist/js/hex.js
@@ -4,10 +4,11 @@ const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener("click", function () {
- let hexColor = '#';
- for(let i = 0; i<6; i++){
-  hexColor += hex[0]
- }
- color.textContent = hexColor;
- document.body.style.backgroundColor = hexColor;
+  let hexColor = "#";
+  for (let i = 0; i < 6; i++) {
+    hexColor += hex[11];
+  }
+
+  color.textContent = hexColor;
+  document.body.style.backgroundColor = hexColor;
 });
